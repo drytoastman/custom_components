bd7248cb7f57ed5e4c74ea286291ac770979c351
Brett Wilson <drytoastman@gmail.com>
2022-05-12 15:00:47 -0700

    split up locksy into components

---
+++ locksy/frontend/src/code-table.ts
@@ -0,0 +1,110 @@
+import { CSSResultGroup, LitElement, css, html } from 'lit';
+import { HomeAssistant, fireEvent } from 'custom-card-helpers';
+import { addNameToLock, deleteCode } from './actions';
+import { customElement, property } from 'lit/decorators.js';
+import { mdiLockPlus, mdiPencil, mdiTrashCanOutline } from '@mdi/js';
+
+import { LocksyData } from './types';
+
+@customElement('code-table')
+export class CodeTable extends LitElement {
+    @property() public hass!: HomeAssistant;
+    @property() public data!: LocksyData;
+
+    private locksWithoutCode(name: string): string[] {
+        return Object.keys(this.data.slots).filter(lockid => !Object.values(this.data.slots[lockid]).includes(name))
+    }
+
+    private unusedCode(name: string): boolean {
+        return Object.keys(this.data.slots).filter(lockid => Object.values(this.data.slots[lockid]).includes(name)).length == 0
+    }
+
+    private getLockName(lockid: string): string {
+        return this.data.entitymap[lockid].name
+    }
+
+    private menu(name: string) {
+        const el = this.shadowRoot?.getElementById(`menu${name}`)
+        if (!el) return;
+        (el as any).show()
+    }
+
+    private addCode(ev: Event) {
+        const element = ev.target as HTMLElement;
+        fireEvent(element, 'show-dialog', {
+          dialogTag: 'name-code-dialog',
+          dialogImport: () => import('./name-code-dialog'),
+          dialogParams: {
+            title: 'New Code',
+            name: '',
+            code: '',
+            change: false,
+            confirm: false
+          },
+        });
+    }
+
+    private changeCode(ev: Event, name: string, code: string) {
+        const element = ev.target as HTMLElement;
+        fireEvent(element, 'show-dialog', {
+          dialogTag: 'name-code-dialog',
+          dialogImport: () => import('./name-code-dialog'),
+          dialogParams: {
+              title: 'Change Code',
+              name: name,
+              code: code,
+              change: true,
+              confirm: false,
+          },
+        });
+    }
+
+
+    render() {
+        return html`
+            <div class="codetable">
+            ${Object.keys(this.data.codes).map(name => html`
+                <div class='name'>${name}</div>
+                <div class='value'>${this.data.codes[name].replace(/./g, '*')}</div>
+                <div style="position: relative;">
+                    ${this.locksWithoutCode(name).length > 0 ? html`
+                        <mwc-button label='' raised dense @click="${() => this.menu(name)}">
+                            <ha-svg-icon path=${mdiLockPlus}></ha-svg-icon>
+                        </mwc-button>
+                        <mwc-menu activatable id="menu${name}" @selected=${(e) => addNameToLock(this.hass, name, e.target.selected.value)}>
+                            ${this.locksWithoutCode(name).map(lockid => html`
+                                <mwc-list-item value=${lockid}>${this.getLockName(lockid)}</mwc-list-item>
+                            `)}
+                        </mwc-menu>
+                    `: html``}
+
+                    <mwc-button raised dense @click=${(e) => this.changeCode(e, name, this.data.codes[name])}>
+                        <ha-svg-icon path=${mdiPencil}></ha-svg-icon>
+                    </mwc-button>
+
+                    ${this.unusedCode(name) ? html`
+                        <mwc-button raised dense @click=${() => deleteCode(this.hass, name)}>
+                            <ha-svg-icon path=${mdiTrashCanOutline}></ha-svg-icon>
+                        </mwc-button>
+                    `: html``}
+                </div>`)}
+            </div>
+            <div class='addwrapper'>
+                <mwc-button class='addbutton' raised dense label="Add Code" @click=${this.addCode}></mwc-button>
+            </div>
+        `;
+    }
+
+    static get styles(): CSSResultGroup {
+        return css`
+            .codetable {
+                display: inline-grid;
+                grid-template-columns: auto auto auto;
+                grid-gap: 1rem;
+                align-items: center;
+                padding: 1rem;
+                padding-top: 0;
+            }
+        `;
+    }
+}
